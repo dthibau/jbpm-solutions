afe19407c3e68a9d439e680d74784b61d1b0a79e
David THI <david.thibau@gmail.com>
2022-01-25 10:25:13 +0100

    Implémentation Cas de test

--- tp2/src/main/java/org/formation/tp2/test/ProcessTest.java
+++ tp2/src/main/java/org/formation/tp2/test/ProcessTest.java
@@ -7,43 +7,71 @@ import org.kie.api.logger.KieRuntimeLogger;
 import org.kie.api.runtime.KieSession;
 import org.kie.api.runtime.manager.RuntimeEngine;
 import org.kie.api.runtime.manager.RuntimeManager;
+import org.kie.api.runtime.manager.audit.AuditService;
 import org.kie.api.runtime.process.ProcessInstance;
-import org.kie.internal.event.KnowledgeRuntimeEventManager;
-import org.kie.internal.logger.KnowledgeRuntimeLogger;
-import org.kie.internal.logger.KnowledgeRuntimeLoggerFactory;
 
 public class ProcessTest extends JbpmJUnitBaseTestCase {
 
 	public ProcessTest() {
-		super(false,false);
+		super(false, false);
 	}
-		@Test
-	   public void testClasspathProcess() {
 
-	       // Créer la session et charger le processus TP2.bpmn à partir du classpath
-	       
-			
+	@Test
+	public void testClasspathProcess() {
+
+		// Créer la session et charger le processus TP2.bpmn à partir du
+		// classpath
+
+		RuntimeManager manager = createRuntimeManager("TP2.bpmn");
+		RuntimeEngine engine = getRuntimeEngine(null);
+		AuditService auditService = engine.getAuditService();
+		KieSession ksession = engine.getKieSession();
+
 		// Associer un logger à la session
-			
-	       _doTests(ksession);
-	       
-	       // Fermer le logger
-	   }
-	   
-		private void _doTests(KieSession ksession) {
-	       // Démarrer le processus 
+		// KnowledgeRuntimeLogger logger =
+		// KnowledgeRuntimeLoggerFactory.newFileLogger(
+		// (KnowledgeRuntimeEventManager)ksession, "tp2" );
+		KieRuntimeLogger logger = KieServices.Factory.get().getLoggers().newThreadedFileLogger(ksession, "tp2", 1000);
 
-	    
-	       // Tester le noeud actif
+		_doTests(ksession);
+		logger.close();
+		manager.close();
+	}
 
-	       
-	       // Tester si le noeud Hello a été déclenché
-	       
-	       // Faire progresser le processus
-	       
-	       // Tester la fin du processus
-	    
-	       // Tester si tous les noeuds ont été déclenchés   
-		   
-	   }
+	@Test
+	public void testGuvnorProcess() throws Exception {
+
+		// Créer la session et charger le processus TP2.bpmn à partir du
+		// repository Guvnor
+		// createRuntimeManager()
+		// Associer un logger à la session
+
+		// _doTests(ksession);
+	}
+
+	private void _doTests(KieSession ksession) {
+		// Démarrer le processus
+
+		ProcessInstance processInstance = ksession.startProcess("org.formation.tp2");
+
+		// Tester le noeud actif
+
+		assertNodeActive(processInstance.getId(), ksession, "Signal");
+
+		// Tester si le noeud Hello a été déclenché
+		assertNodeTriggered(processInstance.getId(), "Hello");
+
+		// Faire progresser le processus
+		ksession.signalEvent("Continue", "None", processInstance.getId());
+
+		// Tester la fin du processus
+//		assertProcessInstanceCompleted(processInstance.getId());
+		assertProcessInstanceCompleted(processInstance.getId(), ksession);
+
+
+		// Tester si tous les noeuds ont été déclenchés
+
+		assertNodeTriggered(processInstance.getId(), "StartProcess", "Hello", "Signal", "EndProcess");
+
+	}
 }
